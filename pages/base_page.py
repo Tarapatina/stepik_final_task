219dd563c830c9f20914d99388fffd38ecc09fda
katerinatarapatina <katzxa@gmail.com>
2020-05-25 19:41:11 +0300

    registretion form and login form

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,14 @@
 #from selenium.webdriver import Remote as RemoteWebDriver
+from selenium.common.exceptions import NoSuchElementException
 class BasePage(object):
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         """Конструктор класса.
                :param browser:
                :param url:
         """
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         """метод открывает нужную страницу,
@@ -14,5 +16,11 @@ class BasePage(object):
         """
         self.browser.get(self.url)
 
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except(NoSuchElementException):
+            return False
+        return True
 
 
